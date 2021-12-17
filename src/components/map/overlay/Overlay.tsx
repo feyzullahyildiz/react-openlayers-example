c02edf265099e3669b8ee8f3733492d441c84575
Feyzullah YILDIZ <ahmet.feyzullah.yildiz@gmail.com>
2021-12-17 10:31:11 +0300

    overlay düzeltildi

--- src/components/map/overlay/Overlay.tsx
+++ src/components/map/overlay/Overlay.tsx
@@ -2,6 +2,7 @@ import { PropsWithChildren, useContext, useEffect, useRef } from 'react'
 import OlOverlay from 'ol/Overlay';
 import { MapContext } from '../MapContext';
 import './Overlay.scss';
+import ReactDOM from 'react-dom';
 interface Props {
     x: number;
     y: number;
@@ -10,13 +11,16 @@ export default function Overlay(props: PropsWithChildren<Props>) {
 
     const map = useContext(MapContext)
     const elRef = useRef(document.createElement('div'));
-    const overlayRef = useRef(new OlOverlay({  }));
+    const overlayRef = useRef(new OlOverlay({}));
     useEffect(() => {
         const overlay = overlayRef.current!;
         map.addOverlay(overlay);
+        document.getElementById('modal-root')!.append(elRef.current)
         overlay.setElement(elRef.current);
         return () => {
             map.removeOverlay(overlay)
+            // eslint-disable-next-line react-hooks/exhaustive-deps
+            elRef.current!.remove();
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
@@ -27,9 +31,11 @@ export default function Overlay(props: PropsWithChildren<Props>) {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [props.x, props.y]);
 
-    return <div className='Overlay' ref={elRef}>
-        {props.children}
-    </div>
-
-
+    
+    return ReactDOM.createPortal(
+        <div className='Overlay'>
+            {props.children}
+        </div>
+        , elRef.current
+    );
 }
